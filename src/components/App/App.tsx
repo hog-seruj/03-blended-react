56d892d6a48fa7801526ef46c7339f97209a9fce
Tetiana Kalkuta <72383361+TanyaKalkuta@users.noreply.github.com>
2025-11-23 17:52:15 +0000

    pagination

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,32 +2,38 @@ import Modal from "../Modal/Modal";
 import PostList from "../PostList/PostList";
 import SearchBox from "../SearchBox/SearchBox";
 import Pagination from "../Pagination/Pagination";
-import { useQuery } from '@tanstack/react-query';
-import { fetchPosts } from '../../services/postService';
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { fetchPosts } from "../../services/postService";
 import { useState } from "react";
 
 import css from "./App.module.css";
 
-export default function App()
-{
-  const [searchText, setSearchText] = useState('');
+export default function App() {
+  const [searchText, setSearchText] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
 
   const { data } = useQuery({
-    queryKey: ['post', searchText, currentPage],
+    queryKey: ["post", searchText, currentPage],
     queryFn: () => fetchPosts(searchText, currentPage),
+    placeholderData: keepPreviousData,
   });
 
   return (
     <div className={css.app}>
       <header className={css.toolbar}>
         <SearchBox />
-        {/* <Pagination /> */}
+        {data && data.totalPages > 1 && (
+          <Pagination
+            currentPage={currentPage}
+            totalPages={data.totalPages}
+            onPageChange={setCurrentPage}
+          />
+        )}
+
         <button className={css.button}>Create post</button>
       </header>
       {/* <Modal></Modal> */}
       {data && <PostList posts={data?.posts} />}
-
     </div>
   );
 }
--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -2,11 +2,16 @@ import ReactPaginate from "react-paginate";
 
 import css from "./Pagination.module.css";
 
-export default function Pagination({ totalPages, currentPage, onPageChange }) {
+interface PaginationProps {
+  totalPages: number;
+  currentPage: number;
+  onPageChange: (selected: number) => void;
+}
+export default function Pagination({ totalPages, currentPage, onPageChange }: PaginationProps) {
   return (
     <ReactPaginate
       pageCount={totalPages}
-      pageRangeDisplayed={5}
+      pageRangeDisplayed={3}
       marginPagesDisplayed={1}
       onPageChange={({ selected }) => onPageChange(selected + 1)}
       forcePage={currentPage - 1}
